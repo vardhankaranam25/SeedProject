bbd03c93955388d7da3a40016399242601c48039
srivardhan karanam <karanam.sr@northeastern.edu>
2024-07-19 15:38:35 +0530

    commit

---
+++ frontend/myapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CreateAdmin from './Components/AdminCreate/CreateAdmin';
+
+
+function App() {
+  return (
+    <div className="App">
+      <CreateAdmin></CreateAdmin>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/myapp/src/Components/AdminCreate/CreateAdmin.js
@@ -0,0 +1,14 @@
+import DrawerAppBar from "./Nav";
+import SimplePaper from "./Paper";
+
+
+
+
+export default function CreateAdmin() {
+    return(
+        <div>
+            <DrawerAppBar></DrawerAppBar>
+            <SimplePaper></SimplePaper>
+        </div>
+    )
+}
