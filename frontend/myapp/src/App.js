fe671eb61499dd27f18143954b06716b298ddcac
Sindhu005 <sindhusingam.10@gmail.com>
2024-07-26 14:47:23 +0530

    changes made in user folder and created css file

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import CreateUser from './Components/ADMIN/AdminCreateUser/CreateUser';
-import Create from './Components/Scientist/Create/Create';
-import Update from './Components/Scientist/Update/Update';
-import CreateAdmin from './Components/ADMIN/AdminCreate/CreateAdmin';
-
+//import CreateUser from './Components/ADMIN/AdminCreateUser/CreateUser';
+//import Create from './Components/Scientist/Create/Create';
+//import Update from './Components/Scientist/Update/Update';
+//import CreateAdmin from './Components/ADMIN/AdminCreate/CreateAdmin';
+import CreateUser from './Components/USER/CreateUser'
 
 function App() {
   return (
     <div className="App">
-      <Update></Update>
+      <CreateUser />
     </div>
   );
 }
--- frontend/myapp/src/Components/ADMIN/AdminCreateUser/CreateUser.js
+++ frontend/myapp/src/Components/ADMIN/AdminCreateUser/CreateUser.js
@@ -1,5 +1,6 @@
 import ResponsiveAppBar from "../AdminCreate/Nav";
 import CreateUserPaper from "./Paper";
+import { GrSomeIcon } from 'react-icons/gr';
 
 
 export default function CreateUser() {
