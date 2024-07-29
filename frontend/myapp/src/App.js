f791e8ddf332fe9bc5ca8ce9912aeb915c100062
ShivaKumarErpula <137309892+ShivaKumarErpula@users.noreply.github.com>
2024-07-29 21:11:27 +0530

    updated app.js file

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import CreateUser from './Components/ADMIN/AdminCreateUser/CreateUser';
-import Create from './Components/Scientist/Create/Create';
-import Update from './Components/Scientist/Update/Update';
+import { Routes,Route } from 'react-router';
 import CreateAdmin from './Components/ADMIN/AdminCreate/CreateAdmin';
+import Create from './Components/Scientist/Create/Create';
+//import Update from './Components/Scientist/Update/Update';
+import LoginPage from './Components/LOGIN/index'
+import CreateUser from './Components/USER/CreateUser'
 
-
-function App() {
-  return (
-    <div className="App">
-      <Update></Update>
-    </div>
-  );
-}
+const App = () => (
+  <Routes>
+    <Route exact path='/login' element={<LoginPage />} />
+    <Route exact path='/user/:username' element={<CreateUser />} />
+    <Route exact path='/admin/:admin' element={<CreateAdmin />} />
+    <Route exact path='/scientist/:scientist' element={<Create />} />
+    <Route exact path='/analyst/:analyst' element={null} />
+  </Routes>
+)
 
 export default App;
